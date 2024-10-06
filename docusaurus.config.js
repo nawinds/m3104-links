148ab88d815c9ea0222bb7885cb58bdfa17a36fa
Nikita Aksenov <me@nawinds.dev>
2024-10-06 15:05:06 +0300

    gtag added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,165 +8,172 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'M3104',
-  favicon: 'img/favicon.ico',
+    title: 'M3104',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://m3104.nawinds.dev',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://m3104.nawinds.dev',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'nawinds', // Usually your GitHub org/user name.
-  projectName: 'm3104-links', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'nawinds', // Usually your GitHub org/user name.
+    projectName: 'm3104-links', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'ru',
-    locales: ['ru'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'ru',
+        locales: ['ru'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: './sidebars.js',
 
-        },
-        /*blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
+                },
+                /*blog: {
+                  showReadingTime: true,
+                  feedOptions: {
+                    type: ['rss', 'atom'],
+                    xslt: true,
+                  },
 
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },*/
-      }),
+                  // Useful options to enforce blogging best practices
+                  onInlineTags: 'warn',
+                  onInlineAuthors: 'warn',
+                  onUntruncatedBlogPosts: 'warn',
+                },
+                theme: {
+                  customCss: './src/css/custom.css',
+                },*/
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      //image: 'img/docusaurus-social-card.jpg',
-      /*navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+        ({
+            // Replace with your project's social card
+            //image: 'img/docusaurus-social-card.jpg',
+            /*navbar: {
+              title: 'My Site',
+              logo: {
+                alt: 'My Site Logo',
+                src: 'img/logo.svg',
+              },
+              items: [
+                {
+                  type: 'docSidebar',
+                  sidebarId: 'tutorialSidebar',
+                  position: 'left',
+                  label: 'Tutorial',
+                },
+                {to: '/blog', label: 'Blog', position: 'left'},
+                {
+                  href: 'https://github.com/facebook/docusaurus',
+                  label: 'GitHub',
+                  position: 'right',
+                },
+              ],
+            },
+            footer: {
+              style: 'dark',
+              links: [
+                {
+                  title: 'Docs',
+                  items: [
+                    {
+                      label: 'Tutorial',
+                      to: '/docs/intro',
+                    },
+                  ],
+                },
+                {
+                  title: 'Community',
+                  items: [
+                    {
+                      label: 'Stack Overflow',
+                      href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                    },
+                    {
+                      label: 'Discord',
+                      href: 'https://discordapp.com/invite/docusaurus',
+                    },
+                    {
+                      label: 'Twitter',
+                      href: 'https://twitter.com/docusaurus',
+                    },
+                  ],
+                },
+                {
+                  title: 'More',
+                  items: [
+                    {
+                      label: 'Blog',
+                      to: '/blog',
+                    },
+                    {
+                      label: 'GitHub',
+                      href: 'https://github.com/facebook/docusaurus',
+                    },
+                  ],
+                },
+              ],
+              copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+            },*/
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+        }),
+    plugins: [
+        [
+            '@docusaurus/plugin-pwa',
+            {
+                debug: true,
+                offlineModeActivationStrategies: [
+                    'appInstalled',
+                    'standalone',
+                    'queryString',
+                ],
+                pwaHead: [
+                    {
+                        tagName: 'link',
+                        rel: 'icon',
+                        href: '/img/android-chrome-512x512.png',
+                    },
+                    {
+                        tagName: 'link',
+                        rel: 'manifest',
+                        href: '/manifest.json', // your PWA manifest
+                    },
+                    {
+                        tagName: 'meta',
+                        name: 'theme-color',
+                        content: 'rgb(255, 255, 255)',
+                    },
+                ],
+            },
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
+        [
+            '@docusaurus/plugin-google-gtag',
+            {
+                trackingID: 'GTM-MVKLVPP2',
+                anonymizeIP: false,
+            },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },*/
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
-  plugins: [
-    [
-      '@docusaurus/plugin-pwa',
-      {
-        debug: true,
-        offlineModeActivationStrategies: [
-          'appInstalled',
-          'standalone',
-          'queryString',
-        ],
-        pwaHead: [
-          {
-            tagName: 'link',
-            rel: 'icon',
-            href: '/img/android-chrome-512x512.png',
-          },
-          {
-            tagName: 'link',
-            rel: 'manifest',
-            href: '/manifest.json', // your PWA manifest
-          },
-          {
-            tagName: 'meta',
-            name: 'theme-color',
-            content: 'rgb(255, 255, 255)',
-          },
-        ],
-      },
     ],
-  ],
 };
 
 export default config;
