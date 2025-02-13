d1e85478be93d581f3a5995e9903d0fc3ec9620b
Nikita Aksenov <nikita.aksenov@nawinds.dev>
2025-02-13 18:11:50 +0300

    swipe fix

--- src/components/SwipeableMenu.js
+++ src/components/SwipeableMenu.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
+import { useSwipeable } from 'react-swipeable';
+import { useThemeConfig } from '@docusaurus/theme-common';
 import { useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
@@ -10,37 +12,31 @@ const SwipeableMenu = ({ children }) => {
       {() => {
         const isMobile = () => /Mobi|Android/i.test(navigator.userAgent);
         const { toggle, shown } = useNavbarMobileSidebar();
-        const swipeRef = useRef(null);
 
-        useEffect(() => {
-          const handleTouchStart = (event) => {
-            swipeRef.current = event.touches[0].clientX;
-          };
-
-          const handleTouchMove = (event) => {
-            if (!swipeRef.current) return;
-
-            const deltaX = event.touches[0].clientX - swipeRef.current;
-
-            if (shown && deltaX < -SWIPE_THRESHOLD) {
-              toggle(); // Закрываем навбар свайпом влево
-            } else if (!shown && deltaX > SWIPE_THRESHOLD) {
-              toggle(); // Открываем навбар свайпом вправо
+        // Обрабатываем свайпы глобально, чтобы работало поверх навбара
+        const handlers = useSwipeable({
+          onSwipedLeft: (eventData) => {
+            eventData.event.stopPropagation();
+            if (shown && Math.abs(eventData.deltaX) > SWIPE_THRESHOLD) {
+              toggle(); // Закрываем навбар
             }
+          },
+          onSwipedRight: (eventData) => {
+            eventData.event.stopPropagation();
+            if (!shown && Math.abs(eventData.deltaX) > SWIPE_THRESHOLD) {
+              toggle(); // Открываем навбар
+            }
+          },
+          trackTouch: true, // Включаем обработку свайпов на мобильных устройствах
+          trackMouse: false, // Отключаем поддержку свайпов мышью
+          preventScrollOnSwipe: true, // Блокируем скролл во время свайпа
+        });
 
-            swipeRef.current = null; // Сбрасываем значение
-          };
-
-          document.body.addEventListener('touchstart', handleTouchStart);
-          document.body.addEventListener('touchmove', handleTouchMove);
-
-          return () => {
-            document.body.removeEventListener('touchstart', handleTouchStart);
-            document.body.removeEventListener('touchmove', handleTouchMove);
-          };
-        }, [shown]);
-
-        return <div>{children}</div>;
+        return (
+          <div {...handlers} style={{ width: '100%', height: '100vh' }}>
+            {children}
+          </div>
+        );
       }}
     </BrowserOnly>
   );
