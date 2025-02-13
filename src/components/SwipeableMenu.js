555f2b0b7a0c36ce060e32370c4b4af3c00f2395
Nikita Aksenov <nikita.aksenov@nawinds.dev>
2025-02-13 17:32:57 +0300

    run failure bugfix

--- src/components/SwipeableMenu.js
+++ src/components/SwipeableMenu.js
@@ -2,35 +2,41 @@ import React from 'react';
 import { useSwipeable } from 'react-swipeable';
 import { useThemeConfig } from '@docusaurus/theme-common';
 import { useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const SWIPE_THRESHOLD = 50; // Минимальная длина свайпа для срабатывания
 
-const isMobile = () => /Mobi|Android/i.test(navigator.userAgent);
-
 const SwipeableMenu = ({ children }) => {
-  const { mobileSidebar } = useThemeConfig();
-  const { toggle, shown } = useNavbarMobileSidebar();
+  return (
+    <BrowserOnly fallback={<>{children}</>}>
+      {() => {
+        const isMobile = () => /Mobi|Android/i.test(navigator.userAgent);
+        const { mobileSidebar } = useThemeConfig();
+        const { toggle, shown } = useNavbarMobileSidebar();
 
-  const handlers = useSwipeable(
-    isMobile()
-      ? {
-          onSwipedLeft: ({ deltaX }) => {
-            if (shown && Math.abs(deltaX) > SWIPE_THRESHOLD) {
-              toggle();
-            }
-          },
-          onSwipedRight: ({ deltaX }) => {
-            if (!shown && Math.abs(deltaX) > SWIPE_THRESHOLD) {
-              toggle();
-            }
-          },
-          trackMouse: false, // Отключаем поддержку свайпов мышью
-          preventScrollOnSwipe: true, // Предотвращает скролл при свайпе
-        }
-      : {}
+        const handlers = useSwipeable(
+          isMobile()
+            ? {
+                onSwipedLeft: ({ deltaX }) => {
+                  if (shown && Math.abs(deltaX) > SWIPE_THRESHOLD) {
+                    toggle();
+                  }
+                },
+                onSwipedRight: ({ deltaX }) => {
+                  if (!shown && Math.abs(deltaX) > SWIPE_THRESHOLD) {
+                    toggle();
+                  }
+                },
+                trackMouse: false, // Отключаем поддержку свайпов мышью
+                preventScrollOnSwipe: true, // Предотвращает скролл при свайпе
+              }
+            : {}
+        );
+
+        return <div {...handlers}>{children}</div>;
+      }}
+    </BrowserOnly>
   );
-
-  return <div {...handlers}>{children}</div>;
 };
 
 export default SwipeableMenu;
