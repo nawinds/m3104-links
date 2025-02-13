725f10d13e05c5a54d71907631f83d07581546ff
Nikita Aksenov <nikita.aksenov@nawinds.dev>
2025-02-13 17:07:26 +0300

    Shortcuts for grade URLs opening

--- src/components/AnchorShortcuts.jsx
+++ src/components/AnchorShortcuts.jsx
@@ -4,34 +4,35 @@ const AnchorShortcuts = () => {
     // Map of single-letter shortcuts to anchor IDs
     const keyToActionMap = {
         // Anchor shortcuts
-        m: {type: "anchor", target: "матан"},
-        s: {type: "anchor", target: "спецразделы-высшей-математики"},
-        d: {type: "anchor", target: "дискретная-математика"},
-        a: {type: "anchor", target: "алгоритмы"},
-        o: {type: "anchor", target: "основы-программирования"},
-        e: {type: "anchor", target: "архитектура-эвм"},
+        m: { type: "anchor", target: "матан", url: "https://docs.google.com/spreadsheets/d/1p0obShDzJhaiLegIOJoWBs_j-evS7sed502BrUB79KY/edit?gid=0#gid=0" },
+        s: { type: "anchor", target: "спецразделы-высшей-математики", url: "https://docs.google.com/spreadsheets/d/14rIVWggg_t8WSs1KiCkotNIL1hQZ7-F_VRpMttsP4bw/edit?gid=0#gid=0" },
+        d: { type: "anchor", target: "дискретная-математика" },
+        a: { type: "anchor", target: "алгоритмы", url: "https://docs.google.com/spreadsheets/d/1DwOhN0JpuUK3i0Zg4rag6J_vBQYGrzjdNWJ20UCTuI8/edit?gid=1919380163#gid=1919380163&range=A104" },
+        o: { type: "anchor", target: "основы-программирования" },
+        e: { type: "anchor", target: "архитектура-эвм", url: "https://docs.google.com/spreadsheets/d/1gDMcROqdYmK-Jxq-6HBT_3PSUs0LcioeHvq0ipQ6jXo/edit?usp=sharing" },
 
-        "м": {type: "anchor", target: "матан"}, // м
-        c: {type: "anchor", target: "спецразделы-высшей-математики"}, // с
-        l: {type: "anchor", target: "дискретная-математика"}, // д
-        f: {type: "anchor", target: "алгоритмы"}, // а
-        j: {type: "anchor", target: "основы-программирования"}, // о
-        ",": {type: "anchor", target: "архитектура-эвм"}, // э - эвм
+
+        "м": { type: "anchor", target: "матан" }, // м
+        c: { type: "anchor", target: "спецразделы-высшей-математики" }, // с
+        l: { type: "anchor", target: "дискретная-математика" }, // д
+        f: { type: "anchor", target: "алгоритмы" }, // а
+        j: { type: "anchor", target: "основы-программирования" }, // о
+        ",": { type: "anchor", target: "архитектура-эвм" }, // э - эвм
 
         // URL shortcuts
-        b: {type: "url", target: "javascript:history.back()"},
-        h: {type: "url", target: "/"},
-        v: {type: "url", target: "/table-grades"},
-        p: {type: "url", target: "/point-distribution"},
+        b: { type: "url", target: "javascript:history.back()" },
+        h: { type: "url", target: "/" },
+        v: { type: "url", target: "/table-grades" },
+        p: { type: "url", target: "/point-distribution" },
 
-        y: {type: "url", target: "javascript:history.back()"}, // н - назад
-        u: {type: "url", target: "/"},   // г - главная
-        g: {type: "url", target: "/point-distribution"}, // п - правила оценивания
+        y: { type: "url", target: "javascript:history.back()" }, // н - назад
+        u: { type: "url", target: "/" },   // г - главная
+        g: { type: "url", target: "/point-distribution" }, // п - правила оценивания
 
         // service shortcuts
-        1: {type: "url", target: "https://github.com/nawinds/m3104-links/edit/master/src/pages/index.mdx"},
-        2: {type: "url", target: "https://github.com/nawinds/m3104-links/edit/master/static/api-deadlines"},
-        3: {type: "url", target: "https://github.com/nawinds/m3104-links/edit/master/src/pages/lecture-recordings.mdx"},
+        1: { type: "url", target: "https://github.com/nawinds/m3104-links/edit/master/src/pages/index.mdx" },
+        2: { type: "url", target: "https://github.com/nawinds/m3104-links/edit/master/static/api-deadlines" },
+        3: { type: "url", target: "https://github.com/nawinds/m3104-links/edit/master/src/pages/lecture-recordings.mdx" },
     };
 
     const russianKeyboardMap = {
@@ -102,10 +103,19 @@ const AnchorShortcuts = () => {
 
             if (action) {
                 if (action.type === "anchor") {
-                    const anchorElement = document.getElementById(action.target);
-                    if (anchorElement) {
-                        smoothScrollTo(anchorElement, 200, 60); // Adjust offset if needed
+                    if (location.pathname === "/table-grades") {
+                        window.open(action.url, "_blank"); // Open link in a new tab
+                    } else {
+                        const anchorElement = document.getElementById(action.target);
+                        if (anchorElement) {
+                            anchorElement.scrollIntoView({behavior: "instant", block: "start"});
+                        }
                     }
+
+                    // const anchorElement = document.getElementById(action.target);
+                    // if (anchorElement) {
+                    //     smoothScrollTo(anchorElement, 200, 60); // Adjust offset if needed
+                    // }
                 } else if (action.type === "url") {
                     window.location.href = action.target; // Redirect to URL in the same tab
                 }
