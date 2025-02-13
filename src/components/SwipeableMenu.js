9160c7a5a40b98761a86bffb140d066570f758fa
Nikita Aksenov <nikita.aksenov@nawinds.dev>
2025-02-13 21:55:24 +0300

    swipe fix

--- src/components/SwipeableMenu.js
+++ src/components/SwipeableMenu.js
@@ -2,21 +2,40 @@ import React from 'react';
 import { useSwipeable } from 'react-swipeable';
 import { useThemeConfig } from '@docusaurus/theme-common';
 import { useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
+const SWIPE_THRESHOLD = 75; // Минимальная длина свайпа для срабатывания
 
 const SwipeableMenu = ({ children }) => {
-  const { mobileSidebar } = useThemeConfig();
-  const { toggle, shown } = useNavbarMobileSidebar();
-
-  const handlers = useSwipeable({
-    onSwipedLeft: () => shown && toggle(), // Закрыть меню при свайпе влево
-    onSwipedRight: () => !shown && toggle(), // Открыть меню при свайпе вправо
-    trackMouse: true, // Для поддержки свайпов мышью
-  });
-
   return (
-    <div {...handlers}>
-      {children}
-    </div>
+    <BrowserOnly fallback={<>{children}</>}>
+      {() => {
+        const isMobile = () => /Mobi|Android/i.test(navigator.userAgent);
+        const { mobileSidebar } = useThemeConfig();
+        const { toggle, shown } = useNavbarMobileSidebar();
+
+        const handlers = useSwipeable(
+          isMobile()
+            ? {
+                onSwipedLeft: ({ deltaX }) => {
+                  if (shown && Math.abs(deltaX) > SWIPE_THRESHOLD) {
+                    toggle();
+                  }
+                },
+                onSwipedRight: ({ deltaX }) => {
+                  if (!shown && Math.abs(deltaX) > SWIPE_THRESHOLD) {
+                    toggle();
+                  }
+                },
+                trackMouse: false, // Отключаем поддержку свайпов мышью
+                preventScrollOnSwipe: true, // Предотвращает скролл при свайпе
+              }
+            : {}
+        );
+
+        return <div {...handlers}>{children}</div>;
+      }}
+    </BrowserOnly>
   );
 };
 
