0bd571896ef90b3207b8f778d66fc19f47a8f300
Nikita Aksenov <nikita.aksenov@nawinds.dev>
2025-04-04 23:13:18 +0300

    varioqub connected

--- static/ya_metrika.js
+++ static/ya_metrika.js
@@ -19,3 +19,10 @@ ym(98560217, "init", {
     webvisor: true
 });
 
+// Varioqub experiments
+(function(e, x, pe, r, i, me, nt){
+e[i]=e[i]||function(){(e[i].a=e[i].a||[]).push(arguments)},
+me=x.createElement(pe),me.async=1,me.src=r,nt=x.getElementsByTagName(pe)[0],me.addEventListener('error',function(){function cb(t){t=t[t.length-1],'function'==typeof t&&t({flags:{}})};Array.isArray(e[i].a)&&e[i].a.forEach(cb);e[i]=function(){cb(arguments)}}),nt.parentNode.insertBefore(me,nt)})
+(window, document, 'script', 'https://abt.s3.yandex.net/expjs/latest/exp.js', 'ymab');
+ymab('metrika.98560217', 'init'/*, {clientFeatures}, {callback}*/);
+
