676d6899c623ade940efff7983c8e94771a5dc88
Nikita Aksenov <nikita.aksenov@nawinds.dev>
2025-04-05 20:08:08 +0300

    new deadline link style hover behaviour

--- src/components/DeadlinesBlock.jsx
+++ src/components/DeadlinesBlock.jsx
@@ -62,8 +62,8 @@ const formatDeadline = (deadline) => {
     if (link) {
         // A/B experiment
         if (deadlineStyle === "new") {
-            text += `<b style="position: relative; display: inline-block;"><a href="${link}" target="_blank" title="Открыть ${deadlineName}" style="text-decoration: none; color: inherit; position: relative; z-index: 1;" onmouseover="this.style.opacity='0.8'" onmouseout="this.style.opacity='1'" onclick="ym(${ym_counter}, 'reachGoal', 'deadline_click'); return true;">${deadlineName}</a>
-                 <span style="position: absolute; bottom: 2px; left: 0; right: 0; height: 1px; background: rgba(157,128,218,0.5); z-index: 0;"></span></b>`;
+            text += `<b style="position: relative; display: inline-block;"><a href="${link}" target="_blank" title="Открыть ${deadlineName}" style="text-decoration: none; color: inherit; position: relative; z-index: 1;" onmouseover="this.parentNode.querySelector('span').style.height='2px'" onmouseout="this.parentNode.querySelector('span').style.height='1px'" onclick="ym(${ym_counter}, 'reachGoal', 'deadline_click'); return true;">${deadlineName}</a>
+                 <span style="position: absolute; bottom: 2px; left: 0; right: 0; height: 1px; background: rgba(157,128,218,0.6); z-index: 0; transition: height 0.1s ease;"></span></b>`;
 
         } else {
             text += `<b style="padding-left: 5px; border-left: 2px solid rgba(157,128,218,0.5);"><a href=\"${link}\" target=\"_blank\" style=\"text-decoration: none; color: inherit;\" onmouseover=\"this.style.opacity='0.8'\" onmouseout=\"this.style.opacity='1'\" onclick="ym(${ym_counter}, 'reachGoal', 'deadline_click'); return true;">${deadlineName}</a></b>`;
@@ -124,7 +124,8 @@ const Deadlines = () => {
         return <p>Загрузка дедлайнов...</p>;
     }
     if (error) {
-        return <p>Error: {error}</p>;
+        console.error(error);
+        return <p>Не удалось загрузить дедлайны.</p>;
     }
     return (
         <div id="deadlinesBlock" style={{ marginBottom: '20px' }}>
